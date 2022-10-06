679067e781beb00dd5ed456a51710b488833c30d
Abhinav Singh <abhinav.singh@mobisy.com>
2022-10-06 14:46:26 +0530

    contentNegotiation, i18-not working

--- restful-web-services/src/main/java/com/example/restwebServices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/example/restwebServices/restfulwebservices/user/UserResource.java
@@ -1,11 +1,14 @@
 package com.example.restwebServices.restfulwebservices.user;
 
 import jakarta.validation.Valid;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class UserResource {
